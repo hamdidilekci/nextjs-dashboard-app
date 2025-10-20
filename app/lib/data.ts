cb7bf703a2b2d6d716aa25b6ea38539e5c190cbd
Hamdi Dilekçi <>
2025-10-20 17:49:08 +0300

    Enhance the invoices page with search functionality and pagination

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,42 +12,46 @@ import { formatCurrency } from './utils';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
-  try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
+	try {
+		// Artificially delay a response for demo purposes.
+		// Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+		console.log('Fetching revenue data...')
+		await new Promise((resolve) => setTimeout(resolve, 1500))
 
-    const data = await sql<Revenue[]>`SELECT * FROM revenue`;
+		const data = await sql<Revenue[]>`SELECT * FROM revenue`
 
-    // console.log('Data fetch completed after 3 seconds.');
+		console.log('Data fetch completed after 1.5 seconds.')
 
-    return data;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch revenue data.');
-  }
+		return data
+	} catch (error) {
+		console.error('Database Error:', error)
+		throw new Error('Failed to fetch revenue data.')
+	}
 }
 
 export async function fetchLatestInvoices() {
-  try {
-    const data = await sql<LatestInvoiceRaw[]>`
+	try {
+		console.log('Fetching latest invoices...')
+		await new Promise((resolve) => setTimeout(resolve, 500))
+		console.log('Latest invoices fetched after 0.5 seconds.')
+
+		const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
-      LIMIT 5`;
+      LIMIT 5`
 
-    const latestInvoices = data.map((invoice) => ({
-      ...invoice,
-      amount: formatCurrency(invoice.amount),
-    }));
-    return latestInvoices;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch the latest invoices.');
-  }
+		const latestInvoices = data.map((invoice) => ({
+			...invoice,
+			amount: formatCurrency(invoice.amount)
+		}))
+		return latestInvoices
+	} catch (error) {
+		console.error('Database Error:', error)
+		throw new Error('Failed to fetch the latest invoices.')
+	}
 }
 
 export async function fetchCardData() {
