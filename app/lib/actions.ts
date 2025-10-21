d4257d8c604d315403f8648cdc64249f661fbd17
Hamdi Dilekçi <>
2025-10-21 19:23:23 +0300

    Refactor deleteInvoice function to throw an error on failure and ensure revalidation of the invoices path

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -102,14 +102,11 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 export async function deleteInvoice(id: string) {
 	try {
 		await sql`DELETE FROM invoices WHERE id = ${id}`
+		revalidatePath('/dashboard/invoices')
 	} catch (error) {
 		console.error(error)
-		return {
-			message: 'Database Error: Failed to Delete Invoice.'
-		}
+		throw new Error('Database Error: Failed to Delete Invoice.')
 	}
-
-	revalidatePath('/dashboard/invoices')
 }
 
 export async function authenticate(prevState: string | undefined, formData: FormData) {
